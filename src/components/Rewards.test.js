ebf3b40b6a9cd328053e2c0f4c90c56c2eced7ba
aditya-vardhan <adityavardhan.goud@gmail.com>
2024-06-21 04:53:53 +0530

    removed unused variable

--- src/components/Rewards.test.js
+++ src/components/Rewards.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Rewards from './Rewards';
 
 test('loads total rewards container', () => {
