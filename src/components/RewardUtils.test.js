26abc99fd8187e80607f2b5a444b2418d5bf20be
adityaVardhan <adityavardhan.goud@gmail.com>
2024-06-26 16:06:23 +0530

    Update RewardUtils.test.js

--- src/components/RewardUtils.test.js
+++ src/components/RewardUtils.test.js
@@ -1,7 +1,7 @@
-import moment from 'moment'
-import { generateRewardsData } from './RewardUtils'
+import moment from 'moment';
+import { generateRewardsData } from './RewardUtils';
 
-test('Adds reward if txn amount is more than 50', () => {
+test('Adds reward if txn amount is more than 100', () => {
   const purchaseData = [
     {
       transaction_id: 'TXN085',
@@ -10,15 +10,15 @@ test('Adds reward if txn amount is more than 50', () => {
       price: 120,
       purchased_date: moment().format('YYYYMMDD')
     }
-  ]
-  const rewardsData = generateRewardsData(purchaseData)
+  ];
+  const rewardsData = generateRewardsData(purchaseData);
   // as txn amount is 120
   // amount above hundred dollars is applicable for 2 reward points, which is 2*20 = 40 points
   // amount between 50 to 100 is applicable for 1 reward point, which is 50 points
   // total reward points is 40 + 50 = 90 points
-  expect(rewardsData.totalUserRewards[0].reward).toBe(90)
-  expect(rewardsData.monthlyUserRewards[0].reward).toBe(90)
-})
+  expect(rewardsData.totalUserRewards[0].reward).toBe(90);
+  expect(rewardsData.monthlyUserRewards[0].reward).toBe(90);
+});
 
 test('Reward is not generated if txn amount not above 50', () => {
   const purchaseData = [
@@ -29,11 +29,11 @@ test('Reward is not generated if txn amount not above 50', () => {
       price: 50,
       purchased_date: moment().format('YYYYMMDD')
     }
-  ]
-  const rewardsData = generateRewardsData(purchaseData)
-  expect(rewardsData.totalUserRewards.length).toBe(0)
-  expect(rewardsData.monthlyUserRewards.length).toBe(0)
-})
+  ];
+  const rewardsData = generateRewardsData(purchaseData);
+  expect(rewardsData.totalUserRewards.length).toBe(0);
+  expect(rewardsData.monthlyUserRewards.length).toBe(0);
+});
 
 test('One point reward is generated for each dollar between 50 and 100 transactions', () => {
   const purchaseData = [
@@ -44,11 +44,11 @@ test('One point reward is generated for each dollar between 50 and 100 transacti
       price: 98,
       purchased_date: moment().format('YYYYMMDD')
     }
-  ]
-  const rewardsData = generateRewardsData(purchaseData)
-  expect(rewardsData.totalUserRewards[0].reward).toBe(48)
-  expect(rewardsData.monthlyUserRewards[0].reward).toBe(48)
-})
+  ];
+  const rewardsData = generateRewardsData(purchaseData);
+  expect(rewardsData.totalUserRewards[0].reward).toBe(48);
+  expect(rewardsData.monthlyUserRewards[0].reward).toBe(48);
+});
 
 test('Reward generated is 50 if txn amount is equals 100', () => {
   const purchaseData = [
@@ -59,11 +59,11 @@ test('Reward generated is 50 if txn amount is equals 100', () => {
       price: 100,
       purchased_date: moment().format('YYYYMMDD')
     }
-  ]
-  const rewardsData = generateRewardsData(purchaseData)
-  expect(rewardsData.totalUserRewards[0].reward).toBe(50)
-  expect(rewardsData.monthlyUserRewards[0].reward).toBe(50)
-})
+  ];
+  const rewardsData = generateRewardsData(purchaseData);
+  expect(rewardsData.totalUserRewards[0].reward).toBe(50);
+  expect(rewardsData.monthlyUserRewards[0].reward).toBe(50);
+});
 
 test('Reward generated within same month for a user is added', () => {
   const purchaseData = [
@@ -81,11 +81,11 @@ test('Reward generated within same month for a user is added', () => {
       price: 100,
       purchased_date: moment().format('YYYYMMDD')
     }
-  ]
-  const rewardsData = generateRewardsData(purchaseData)
-  expect(rewardsData.totalUserRewards[0].reward).toBe(100)
-  expect(rewardsData.monthlyUserRewards[0].reward).toBe(100)
-})
+  ];
+  const rewardsData = generateRewardsData(purchaseData);
+  expect(rewardsData.totalUserRewards[0].reward).toBe(100);
+  expect(rewardsData.monthlyUserRewards[0].reward).toBe(100);
+});
 
 test('Seperate reward records are created for a user for txns done in different months', () => {
   const purchaseData = [
@@ -103,13 +103,13 @@ test('Seperate reward records are created for a user for txns done in different 
       price: 100,
       purchased_date: moment().subtract(1, 'months').format('YYYYMMDD')
     }
-  ]
-  const rewardsData = generateRewardsData(purchaseData)
-  expect(rewardsData.totalUserRewards[0].reward).toBe(100)
-  expect(rewardsData.monthlyUserRewards[0].reward).toBe(50)
-  expect(rewardsData.monthlyUserRewards[1].reward).toBe(50)
-  expect(rewardsData.monthlyUserRewards.length).toBe(2)
-})
+  ];
+  const rewardsData = generateRewardsData(purchaseData);
+  expect(rewardsData.totalUserRewards[0].reward).toBe(100);
+  expect(rewardsData.monthlyUserRewards[0].reward).toBe(50);
+  expect(rewardsData.monthlyUserRewards[1].reward).toBe(50);
+  expect(rewardsData.monthlyUserRewards.length).toBe(2);
+});
 
 test('Seperate reward records are created for every user', () => {
   const purchaseData = [
@@ -134,14 +134,14 @@ test('Seperate reward records are created for every user', () => {
       price: 100,
       purchased_date: moment().format('YYYYMMDD')
     }
-  ]
-  const rewardsData = generateRewardsData(purchaseData)
-  expect(rewardsData.totalUserRewards.length).toBe(3)
-  expect(rewardsData.monthlyUserRewards.length).toBe(3)
-  expect(rewardsData.totalUserRewards[0].reward).toBe(50)
-  expect(rewardsData.totalUserRewards[1].reward).toBe(50)
-  expect(rewardsData.totalUserRewards[2].reward).toBe(50)
-  expect(rewardsData.monthlyUserRewards[0].reward).toBe(50)
-  expect(rewardsData.monthlyUserRewards[1].reward).toBe(50)
-  expect(rewardsData.monthlyUserRewards[2].reward).toBe(50)
-})
+  ];
+  const rewardsData = generateRewardsData(purchaseData);
+  expect(rewardsData.totalUserRewards.length).toBe(3);
+  expect(rewardsData.monthlyUserRewards.length).toBe(3);
+  expect(rewardsData.totalUserRewards[0].reward).toBe(50);
+  expect(rewardsData.totalUserRewards[1].reward).toBe(50);
+  expect(rewardsData.totalUserRewards[2].reward).toBe(50);
+  expect(rewardsData.monthlyUserRewards[0].reward).toBe(50);
+  expect(rewardsData.monthlyUserRewards[1].reward).toBe(50);
+  expect(rewardsData.monthlyUserRewards[2].reward).toBe(50);
+});
