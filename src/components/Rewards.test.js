eaa11cba0bb6e05b468b60831cb510e716d2f413
adityaVardhan <adityavardhan.goud@gmail.com>
2024-06-26 16:05:48 +0530

    Update Rewards.test.js

--- src/components/Rewards.test.js
+++ src/components/Rewards.test.js
@@ -1,12 +1,12 @@
-import { render } from '@testing-library/react'
-import Rewards from './Rewards'
+import { render } from '@testing-library/react';
+import Rewards from './Rewards';
 
 test('loads total rewards container', () => {
-  const { container } = render(<Rewards />)
-  expect(container.querySelector('.total-rewards')).not.toBeNull()
-})
+  const { container } = render(<Rewards />);
+  expect(container.querySelector('.total-rewards')).not.toBeNull();
+});
 
 test('loads total rewards container', () => {
-  const { container } = render(<Rewards />)
-  expect(container.querySelector('.monthly-rewards')).not.toBeNull()
-})
+  const { container } = render(<Rewards />);
+  expect(container.querySelector('.monthly-rewards')).not.toBeNull();
+});
