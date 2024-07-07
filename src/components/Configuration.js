c8cb59d5b3eb602a46370b96ca995828a07437c9
aditya-vardhan <adityavardhan.goud@gmail.com>
2024-07-07 20:29:09 +0530

    added disable button

--- src/components/Configuration.js
+++ src/components/Configuration.js
@@ -7,11 +7,28 @@ function Configuration(props) {
     const [onePointThreshold, setOnePointThreshold] = useState(rewardCriteria.onePointReward)
     const [twoPointThreshold, setTwoPointThreshold] = useState(rewardCriteria.twoPointReward)
 
-    const changeCriteria = () => {
+    const changeCriteria = (onePointThreshold, twoPointThreshold) => {
+        console.log('eeeee')
+        const updatedOnePointThreshold = Number(onePointThreshold);
+        const updatedTwoPointThreshold = Number(twoPointThreshold);
         setRewardCriteria({
-            onePointReward: onePointThreshold,
-            twoPointReward: twoPointThreshold
+            onePointReward: updatedOnePointThreshold,
+            twoPointReward: updatedTwoPointThreshold
         })
+    }
+
+    const isDisabled = (rewardThreshold1, rewardThreshold2) => {
+        const updatedOnePointThreshold = Number(rewardThreshold1);
+        const updatedTwoPointThreshold = Number(rewardThreshold2);
+        // if user entered other than number
+        if(isNaN(updatedOnePointThreshold) || isNaN(updatedTwoPointThreshold)){
+            return true
+        }
+        // if one point threshold is greater than two point threshold
+        if(updatedOnePointThreshold > updatedTwoPointThreshold){
+            return true
+        }
+        return false
     }
 
     return <div>
@@ -21,18 +38,19 @@ function Configuration(props) {
         <form method="get" action="javascript: void(0);" id="config-form" className="login-form" autoComplete="off" role="main">
             <div>
                 <label className="label-one-point">
-                    <input type="one-point" className="text" value={onePointThreshold} onChange={(e) => setOnePointThreshold(e.target.value)} name="one-point" placeholder="One Point Threshold" tabIndex="1" required />
+                    <input type="one-point" className="text" value={onePointThreshold} onChange={(e) => setOnePointThreshold(e.target.value)} name="one-point" placeholder="One Point Threshold" tabIndex="1" />
                     <span className="required">One Point Threshold</span>
                 </label>
             </div>
 
             <div>
                 <label className="label-two-point">
-                    <input type="text" className="text" value={twoPointThreshold} onChange={(e) => setTwoPointThreshold(e.target.value)} name="two-point" placeholder="Two Point Threshold" tabIndex="2" required />
+                    <input type="text" className="text" value={twoPointThreshold} onChange={(e) => setTwoPointThreshold(e.target.value)} name="two-point" placeholder="Two Point Threshold" tabIndex="2" />
                     <span className="required">Two Point Threshold</span>
                 </label>
             </div>
-            <input type="submit" value="Submit" onClick={changeCriteria} />
+            <input type="submit" value="Submit" disabled={isDisabled(onePointThreshold, twoPointThreshold)}
+              onClick={() => changeCriteria(onePointThreshold, twoPointThreshold)} />
 
         </form>
 
